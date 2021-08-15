aac1a15e9912ccca18ac35207f7b77a86754ff3f
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2021-08-15 18:19:49 +0530

    Create Java_Mini_Project.java

---
+++ Java_Mini_Project.java
@@ -0,0 +1,36 @@
+package com.Dileep;
+
+import java.util.Scanner;
+
+public class Java_Mini_Project {
+
+    public static void main(String args[])
+    {
+        //Mini Project -> checking Random Number(System Generated Number) is Equal to user given Number
+        Scanner sc = new Scanner(System.in);
+        int myNumber = (int)(Math.random()*100);
+
+        int userNumber = 0;
+
+        do {
+            System.out.println("Guess my number(1-100) : ");
+            userNumber = sc.nextInt();
+            if(userNumber == myNumber) {
+                System.out.println("WOOHOO...CORRECT NUMBER!!!");
+                break;
+            }
+            else if(userNumber > myNumber) {
+                System.out.println("your number is too large");
+            }
+
+            else {
+                System.out.println("Your number is too small");
+            }
+
+        }while(userNumber >= 0);
+
+        System.out.print("My number was: ");
+        System.out.println(userNumber);
+
+    }
+}
