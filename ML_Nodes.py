e6fef8a709ca00520f4a798d6daef59329242e68
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-05-11 11:28:03 +0530

    Create ML_Nodes.py

---
+++ ML_Nodes.py
@@ -0,0 +1,28 @@
+'''
+the kernel trick
+means without applying the high dimension space we are getting a non linear decision boundary that is called kernel trick
+types of kernels
+1)Gauusian Kernal
+2)Sigmoid Kernel
+3)Polynomial Kernel
+
+Non-Linear SVR
+
+Section SVR
+
+Section on SVM
+
+Section on Kernel Trick
+1)Kernel SVM Intuition
+2)Mapping to a higher dimension
+3)The Kernel Trick
+4)Types of Kernel Functions
+5)Non-Linear Kernel SVR
+
+Non Linear SVM
+it is caomplex problem we are sovling
+Going in to a 3rd Dimensions and doing Hyperplane and getting out our Results.
+
+rbf kernal -> radial basis funtional kernel 
+
+'''
