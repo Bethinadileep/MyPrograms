fd613661bff9dd32644c52def5062821a23bcf5d
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-06-17 10:51:02 +0530

    Create ternary_operator.c

---
+++ ternary_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 0;
+    //ternary operator
+    age > 18? printf("You are Adult\n") : printf("Yor are not Adult\n");
+    
+    int num = 7;
+    
+    num == 7 ? printf("Your are Lucky") : printf("Your are not Lucky");
+    return 0;
+}
